d581a78ac68bdef899e53d5b79def0fd0351784e
InfiBeyond <nanxinqin@hotmail.com>
2020-08-16 15:01:04 -0700

    Upload

---
+++ app/src/main/java/com/nanxinqin/checkr/DTO/ToDo.kt
@@ -0,0 +1,12 @@
+package com.nanxinqin.checkr.DTO
+
+
+class ToDo {
+
+    var id: Long = -1
+    var name = ""
+    var createdAt = ""
+    var items: MutableList<ToDoItem> = ArrayList()
+
+}
+
---
+++ app/src/main/java/com/nanxinqin/checkr/DTO/ToDoItem.kt
@@ -0,0 +1,11 @@
+package com.nanxinqin.checkr.DTO
+
+class ToDoItem(){
+
+    var id : Long = -1
+    var toDoId : Long = -1
+    var itemName = ""
+    var isCompleted = false
+
+}
+
